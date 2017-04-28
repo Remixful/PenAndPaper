c129bacfc324b4bc879bce12ec99ee7e2a1e3888
Remi <remixfuldev@gmail.com>
2017-04-27 23:31:04 -0400

    You're no longer a plugin.... BE FREEEEEEEE

--- src/main/java/me/remixful/penandpaper/PenAndPaper.java
+++ src/main/java/me/remixful/penandpaper/PenAndPaper.java
@@ -1,7 +1,6 @@
 package me.remixful.penandpaper;
 
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 
 
 /**
@@ -9,7 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
  *
  * @see <a href="https://github.com/Remixful/PenAndPaper">Official GitHub</a>
  */
-public class PenAndPaper extends JavaPlugin {
+public class PenAndPaper {
     /**
      * New book written book.
      *
